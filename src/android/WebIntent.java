b8e83c61d03b063ffb3a5da4260bc08d9c607bd3
Joshua Elson <josh@fluentstream.com>
2015-08-29 23:17:00 -0600

    Add debug around activity

--- src/android/WebIntent.java
+++ src/android/WebIntent.java
@@ -105,6 +105,10 @@ public class WebIntent extends CordovaPlugin {
                 }
                 Intent i = ((CordovaActivity)this.cordova.getActivity()).getIntent();
                 String tel = i.getStringExtra(Intent.EXTRA_PHONE_NUMBER).toString();
+		String phonenumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+		Log.w("", "We are in activity: " + this.cordova.getActivity().toString());
+		Log.w("", "FluentCloud Number To Dial ----> " + tel);
+		Log.w("", "FluentCloud Phone Number To Dial ----> " + phonenumber);
                 callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, tel));
                 return true;
             } else if (action.equals("getUri")) {
